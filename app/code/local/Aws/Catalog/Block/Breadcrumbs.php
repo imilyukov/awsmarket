63009b7f8e7fe262e0294140e133c83de4315026
imilyukov <imilyukov@imilyukov-desktop.(none)>
2013-04-30 05:04:04 +0300

    Add registration.

---
+++ app/code/local/Aws/Catalog/Block/Breadcrumbs.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: imilyukov
+ * Date: 4/30/13
+ * Time: 1:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Aws_Catalog_Block_Breadcrumbs extends Mage_Catalog_Block_Breadcrumbs
+{
+
+}
