79c6e591a8875a4b0a657eb26d3133bcbc9b8fc3
imilyukov <imilyukov@imilyukov-desktop.(none)>
2013-05-15 00:35:00 +0300

    Add product popup.

--- skin/frontend/awsmarket/default/js/script.js
+++ skin/frontend/awsmarket/default/js/script.js
@@ -70,9 +70,83 @@
         // add handler for filter form
         $('#sup__form_filter select').on('change', function(e) {
 
-            console.log($(this).val());
             $(this).closest('form').trigger('submit');
         });
 
+        // pop - up
+        $('.promo a, .prodList a, a.product-image').on('click', function popupHandler(e) {
+
+            $('.close-popup').trigger('click');
+            var url = $(this).attr('href');
+            $('<div />', {class: 'overlay'}).appendTo('body');
+            $.ajax({
+                url: url,
+                success: function(data) {
+
+                    $('body').append(data);
+
+                    var context = $('.popup');
+
+                    $('.close-popup', context).on('click', function(e) {
+
+                        $(context).remove();
+                        $('.overlay').remove();
+                        e.preventDefault();
+                    });
+
+                    $('.select-color-btn, .select-size-btn', context).on('click', popupHandler);
+
+                    if ( !$('.additional-popup-images a', context).length ) {return;}
+
+                    $(".additional-popup-images > div", context).jCarouselLite({
+                        btnNext: ".next-img",
+                        btnPrev: ".prev-img",
+                        vertical: false,
+                        visible: 3,
+                        circular: false
+                    });
+
+                    var mainContainer = $('.main-popup-image > div', context), iviewer = {},
+                        src = $('img', mainContainer).attr('src');
+
+                    $('img', mainContainer).remove();
+                    mainContainer.iviewer({
+                        src: src,
+                        ui_disabled: true,
+                        zoom: 100,
+                        onStartDrag: function() {
+
+                        },
+                        onDrag: function() {
+
+                        },
+                        initCallback: function(){
+
+                            iviewer = this;
+                            $(".enlarge-plus", context).on('click.zoom', function(){ iviewer.zoom_by(1);});
+                            $(".enlarge-minus", context).on('click.zoom', function(){ iviewer.zoom_by(-1);});
+                            $(".full-screen-btn", context).on('click.zoom', function(){ iviewer.set_zoom(100); });
+                        }
+                    });
+
+                    $('.additional-popup-images a', context).on('click', function(e) {
+
+                        var self = this, uri = $(self).attr('href'), index = $(self).data('index');
+
+                        iviewer.loadImage(uri);
+
+                        $('.full-screen-btn', context).attr('href', uri);
+
+                        // counter-img
+                        $('.counter-img', context).text(index + '/' + $('.additional-popup-images a', context).length);
+
+                        e.preventDefault();
+                        e.stopPropagation();
+                    }).eq(0).trigger('click');
+                },
+                cache: true
+            });
+            e.preventDefault();
+        });
     });
 })(jQuery);
